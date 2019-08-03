8c24a8cc91985411b1b4f53216969258af4c61a2
Rodpoolazo <rodpoolazo@gmail.com>
2019-08-03 10:54:40 -0400

    Add Devise

---
+++ db/schema.rb
@@ -0,0 +1,61 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_08_03_145216) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "realties", force: :cascade do |t|
+    t.integer "commune_id"
+    t.string "street"
+    t.integer "number_unit"
+    t.string "unit_estate"
+    t.integer "street_type_id"
+    t.string "population_villa"
+    t.integer "condominium_id"
+    t.integer "property_type"
+    t.integer "apple"
+    t.integer "property"
+    t.text "name_realty"
+    t.integer "latitude"
+    t.integer "longitude"
+    t.float "mt2_land"
+    t.float "mt2_built"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "service_accounts", force: :cascade do |t|
+    t.integer "light"
+    t.integer "water"
+    t.integer "gas"
+    t.integer "common_expense"
+    t.integer "contribution"
+    t.integer "garbage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
